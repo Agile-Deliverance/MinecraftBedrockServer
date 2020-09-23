ad60042eaed66cf2d1dce44992d2609dcabb7e1b
Rich Birch <ringo@albumsnaps.com>
2020-09-23 19:41:30 +0100

    Clear screen scrollback before each list of players to prevent large output files

--- checkIdleTime.sh
+++ checkIdleTime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo -u ubuntu screen -r BlingMyMc -p0 -X eval "clear"
+sudo -u ubuntu screen -r BlingMyMc -p0 -X eval "scrollback 0"
+sudo -u ubuntu screen -r BlingMyMc -p0 -X eval "scrollback 1000"
 sudo -u ubuntu screen -r BlingMyMc -p0 -X stuff "list"
 sudo -u ubuntu screen -r BlingMyMc -p0 -X eval "stuff \015"
 sleep 1
